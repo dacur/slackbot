e0d381b1c14d9d25d5bbe7ae31f73b3beb28ce56
Keegan Brown <brown.keegan@gmail.com>
2016-02-05 15:22:26 -0600

    Sends JSON

--- spec/api.github_spec.js
+++ spec/api.github_spec.js
@@ -1,18 +1,13 @@
 'use strict';
-var config;
-try {
-  config = require('../config.json');
-} catch (e) {
-  config = require('../config.example.json');
-}
 
 const request = require('request'),
     base_url = 'http://localhost:3000/',
     CrBot = require('../lib/crbot.js'),
-    crbot = new CrBot(config);
+    crbot = new CrBot(require('../config.json'));
 
 describe('CRBot Github API', () => {
   let server;
+
   beforeAll((done) => {
     server = crbot.app.listen(3000, () => { done(); });
   });
@@ -24,17 +19,17 @@ describe('CRBot Github API', () => {
   describe('Demonstrate Getting PR Data from Github API', () => {
     it('Returns Data with .url param that matches the request params.', (done) => {
       let prReq = {
-          'user': 'smashingboxes',
-          'repo': 'code-review-bot',
-          'number' : '1'
+        'user': 'smashingboxes',
+        'repo': 'code-review-bot',
+        'number' : '1'
       };
 
       crbot.getPR(prReq).then((promises) => {
-        //console.log(promises);
         expect(promises[0].url).toBe(`https://api.github.com/repos/${prReq.user}/${prReq.repo}/pulls/${prReq.number}`);
         done();
       }).catch((err) => {
-        console.log('ERRR',err);
+        expect(false).toBe(true);
+        done();
       });
     });
   });
--- spec/crbot_spec.js
+++ spec/crbot_spec.js
@@ -1,31 +1,34 @@
-var request = require("request"),
-    base_url = "http://localhost:3000/",
-    CrBot = require("../lib/crbot.js"),
-    crbot = new CrBot({ token: "token", name: "bot" });
+'use strict';
 
-describe("CRBot Server", () => {
-  var server;
+const request = require('request'),
+    base_url = 'http://localhost:3000/',
+    CrBot = require('../lib/crbot.js'),
+    crbot = new CrBot(require('../config.json'));
+
+describe('CRBot Server', () => {
+  let server, formData;
+
   beforeAll((done) => {
     server = crbot.app.listen(3000, () => { done(); });
   });
 
   afterAll(() => {
-    server.close()
+    server.close();
   });
 
-  describe("POST /code_review", () => {
-    it("returns 200", (done) => {
+  describe('POST /code_review', () => {
+    it('returns 200', (done) => {
       formData = {
-        token: "hfu1x6iRs4Hv3kfywP5YV65j",
-        team_id: "T0001",
-        team_domain: "example",
-        channel_id: "C2147483705",
-        channel_name: "test",
-        user_id: "U2147483697",
-        user_name: "Steve",
-        command: "/cr",
-        text: "github.com/smashingboxes/cr-bot/pulls/1",
-        response_url: "https://hooks.slack.com/commands/1234/5678"
+        token: 'hfu1x6iRs4Hv3kfywP5YV65j',
+        team_id: 'T0001',
+        team_domain: 'example',
+        channel_id: 'C2147483705',
+        channel_name: 'test',
+        user_id: 'U2147483697',
+        user_name: 'Steve',
+        command: '/cr',
+        text: 'github.com/smashingboxes/code-review-bot/pulls/1',
+        response_url: 'https://hooks.slack.com/commands/1234/5678'
       }
 
       request.post(base_url + 'code_review', { form: formData }, (error, response, body) => {
